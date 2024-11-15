3fd7ccf17957c0baea5ef7c3c237ef59da43c98f
UnKnowMQ <buiminhquan300304@gmail.com>
2024-11-15 22:43:12 +0700

    add checkout

---
+++ src/Checkout/Checkout.jsx
@@ -0,0 +1,150 @@
+import React from "react"
+import 'bootstrap/dist/css/bootstrap.css';
+import { Link } from "react-router-dom";
+const Checkout = () =>{
+    return(
+
+<div class="container">
+<div className="row">
+        <div className="col-md-4 order-md-2 mb-4">
+          <h4 className="d-flex justify-content-between align-items-center mb-3">
+            <span className="text-muted">Giỏ hàng</span>
+            <span className="badge badge-secondary badge-pill">3</span>
+          </h4>
+          <ul className="list-group mb-3">
+            <li className="list-group-item d-flex justify-content-between lh-condensed">
+              <div>
+                <h6 className="my-0">Product name</h6>
+                <small className="text-muted">Brief description</small>
+              </div>
+              <span className="text-muted">$12</span>
+            </li>
+            <li className="list-group-item d-flex justify-content-between lh-condensed">
+              <div>
+                <h6 className="my-0">Second product</h6>
+                <small className="text-muted">Brief description</small>
+              </div>
+              <span className="text-muted">$8</span>
+            </li>
+            <li className="list-group-item d-flex justify-content-between lh-condensed">
+              <div>
+                <h6 className="my-0">Third item</h6>
+                <small className="text-muted">Brief description</small>
+              </div>
+              <span className="text-muted">$5</span>
+            </li>
+            <li className="list-group-item d-flex justify-content-between bg-light">
+              <div className="text-success">
+                <h6 className="my-0">Mã thẻ khuyến mại</h6>
+                <small>EXAMPLECODE</small>
+              </div>
+              <span className="text-success">-$5</span>
+            </li>
+            <li className="list-group-item d-flex justify-content-between">
+              <span>Total (USD)</span>
+              <strong>$20</strong>
+            </li>
+          </ul>
+          <form className="card p-2">
+            <div className="input-group">
+              <input type="text" className="form-control" placeholder="Khuyến mại"/>
+              <div className="input-group-append">
+                <button type="submit" className="btn btn-secondary">Áp dụng</button>
+              </div>
+            </div>
+          </form>
+        </div>
+        
+          <div className="col-md-8 order-md-1">
+          <h4 className="mb-3 ms-1">Địa Chỉ</h4>
+          <form className="needs-validation" novalidate>
+            <div className="row ps-0">
+              <div className="col mb-3">
+                <label for="firstName ">First name</label>
+                <input type="text" className="form-control" id="firstName" placeholder="" value="" required/>
+                <div className="invalid-feedback">
+                  Valid first name is required.
+                </div>
+              </div>
+              <div className="col mb-3">
+                <label for="lastName">Last name</label>
+                <input type="text" className="form-control" id="lastName" placeholder="" value="" required/>
+                <div className="invalid-feedback">
+                  Valid last name is required.
+                </div>
+              </div>
+            </div>
+
+            <div className="mb-3">
+              <label for="username">Username</label>
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text">@</span>
+                </div>
+                <input type="text" className="form-control" id="username" placeholder="Username" required/>
+                <div className="invalid-feedback" style={{width:"100%"}}>
+                  Your username is required.
+                </div>
+              </div>
+            </div>
+
+            <div className="mb-3">
+              <label for="email">Email <span className="text-muted">(Optional)</span></label>
+              <input type="email" className="form-control" id="email" placeholder="you@example.com"/>
+              <div className="invalid-feedback">
+                Please enter a valid email address for shipping updates.
+              </div>
+            </div>
+
+            <div className="mb-3">
+              <label for="address">Address</label>
+              <input type="text" className="form-control" id="address" placeholder="1234 Main St" required/>
+              <div className="invalid-feedback">
+                Please enter your shipping address.
+              </div>
+            </div>
+
+            <div className="mb-3">
+              <label for="address2">Address 2 <span className="text-muted">(Optional)</span></label>
+              <input type="text" className="form-control" id="address2" placeholder="Apartment or suite"/>
+            </div>
+
+           
+            <hr className="mb-4"/>
+            <div className="custom-control custom-checkbox">
+              <input type="checkbox" className="custom-control-input" id="same-address"/>
+              <label className="custom-control-label" for="same-address">Shipping address is the same as my billing address</label>
+            </div>
+            <div className="custom-control custom-checkbox">
+              <input type="checkbox" className="custom-control-input" id="save-info"/>
+              <label className="custom-control-label" for="save-info">Save this information for next time</label>
+            </div>
+            <hr className="mb-4"/>
+
+            <h4 className="mb-3 ms-0">Phương thức thanh toán</h4>
+
+            <div className="d-block my-3">
+              <div className="custom-control custom-radio">
+                <input id="QR" name="paymentMethod" type="radio" className="custom-control-input" checked required/>
+                <label className="custom-control-label" for="QR">QR Pay</label>
+              </div>
+              <div className="custom-control custom-radio">
+                <input id="COD" name="paymentMethod" type="radio" className="custom-control-input" required/>
+                <label className="custom-control-label" for="COD">COD</label>
+              </div>
+            </div>
+
+            <hr className="mb-4"/>
+            <Link to="../Checkout/QR">
+            <button className="btn btn-primary btn-lg btn-block" type="submit">Tiếp tục</button>
+            </Link>
+          </form>
+        </div>
+      </div>
+        </div>
+        
+        
+
+    )
+}
+export default Checkout
---
+++ src/Checkout/QRPay.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import QRcode from "../assets/QR.png"
+import { width } from "@fortawesome/free-brands-svg-icons/fa42Group";
+const QR = () =>{
+    return (
+        <div className="row"> 
+        <div className="col">
+            <h4>Thông tin của bạn</h4>
+            <p>Họ và Tên: </p>
+            <p>Địa chỉ: </p>
+            <p>Phương thức thanh toán: QR Pay</p>
+            <p>Tổng số tiền: </p>
+        </div>
+        <div className="col">
+            <p className="text-center">Quét mã QR dưới đây bằng ứng dụng internet banking để thanh toán</p>
+            <p className="text-center">Mã QR sẽ hết hạn sau 24h</p>
+            <img className="rounded mx-auto d-block" src={QRcode} alt="" style={{width:"30%"}}/>
+            <p className="mt-2 text-center">Mã hoá đơn: 2000000343</p>
+            <div className="text-center">
+            <button className="btn btn-primary " type="submit">Tôi đã hoàn tất thanh toán</button>
+            </div>
+            
+        </div>
+        </div>
+    )
+}
+export default QR
